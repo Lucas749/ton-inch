c566b551994801ede12381dbdd57a3345d0d9f69
Luke749 <48569945+Lucas749@users.noreply.github.com>
2025-08-02 22:06:13 +0100

    Remove all Alpha Vantage market intelligence widgets per user request

--- front-end/components/widgets/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all widgets for easy importing
-export { default as MarketNewsWidget } from './MarketNewsWidget';
-export { default as TopMoversWidget } from './TopMoversWidget';
-export { default as MarketStatusWidget } from './MarketStatusWidget';
-export { default as TechnicalIndicatorWidget } from './TechnicalIndicatorWidget';
-export { default as InsiderTransactionsWidget } from './InsiderTransactionsWidget';
